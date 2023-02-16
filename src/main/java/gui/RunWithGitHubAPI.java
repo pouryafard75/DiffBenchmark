bcc573175add50eb83b92b1aaf5533ac742e815c
tsantalis <tsantalis@gmail.com>
2023-02-16 07:50:22 -0500

    formatting

--- src/main/java/gui/RunWithGitHubAPI.java
+++ src/main/java/gui/RunWithGitHubAPI.java
@@ -1,6 +1,5 @@
 package gui;
 
-
 import org.refactoringminer.api.RefactoringMinerTimedOutException;
 import org.refactoringminer.astDiff.actions.ASTDiff;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;

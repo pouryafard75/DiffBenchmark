7d4f5755d9c75056a8bb33628a09e457e425a1e3
Pouryafard75 <pouryafard75@gmail.com>
2023-04-17 10:12:40 -0400

    Add DiffStats class

---
+++ src/benchmark/Oracle/DiffStats.java
@@ -0,0 +1,21 @@
+package benchmark.Oracle;
+
+import java.util.List;
+
+/* Created by pourya on 2023-04-16 2:42 a.m. */
+public class DiffStats {
+    private final Stats programElementStats;
+    private final Stats abstractMappingStats;
+    public DiffStats(Stats programElementStats, Stats abstractMappingStats) {
+        this.programElementStats = programElementStats;
+        this.abstractMappingStats = abstractMappingStats;
+    }
+
+    public Stats getProgramElementStats() {
+        return programElementStats;
+    }
+
+    public Stats getAbstractMappingStats() {
+        return abstractMappingStats;
+    }
+}

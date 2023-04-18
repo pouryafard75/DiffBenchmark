0fd94de1853395177acea60b63467427ac8f97db
Pouryafard75 <pouryafard75@gmail.com>
2023-04-18 18:15:22 -0400

    Fix the Javadoc typo

--- src/benchmark/utils/AdditionalASTConstants.java
+++ src/benchmark/utils/AdditionalASTConstants.java
@@ -1,7 +1,10 @@
 package benchmark.utils;
 
+import org.refactoringminer.astDiff.matchers.Constants;
+import shaded.org.eclipse.jdt.internal.compiler.impl.Constant;
+
 /* Created by pourya on 2023-04-18 12:38 p.m. */
 public class AdditionalASTConstants {
     public static final String COMPILATION_UNIT = "CompilationUnit";
-    public static final String JAVA_DOC = "JavaDoc";
+    public static final String JAVA_DOC = "Javadoc";
 }

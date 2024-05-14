9a9792484e05d4b57c20d7840f147d477b1a4c32
Pouryafard75 <pouryafard75@gmail.com>
2024-05-13 20:25:00 -0400

    Show the tool name in the GUI panel

--- src/main/java/benchmark/gui/CompareWithGitHubAPI.java
+++ src/main/java/benchmark/gui/CompareWithGitHubAPI.java
@@ -8,9 +8,9 @@ public class CompareWithGitHubAPI {
     public static void main(String[] args) throws Exception {
         String url;
         url = "https://github.com/Alluxio/alluxio/commit/9aeefcd8120bb3b89cdb437d8c32d2ed84b8a825";
+        url = "https://github.com/phishman3579/java-algorithms-implementation/commit/ab98bcacf6e5bf1c3a06f6bcca68f178f880ffc9";
 //        url = "https://github.com/brettwooldridge/HikariCP/commit/1571049ec04b1e7e6f082ed5ec071584e7200c12";
-//        url = "https://github.com/Alluxio/alluxio/commit/0ba343846f21649e29ffc600f30a7f3e463fb24c";
-        url = "https://github.com/Athou/commafeed/commit/18a7bd1fd1a83b3b8d1b245e32f78c0b4443b7a7";
+//        url = "https://github.com/facebook/buck/commit/84b7b3974ae8171a4de2f804eb94fcd1d6cd6647";
 
 
         BenchmarkWebDiff benchmarkWebDiff = BenchmarkWebDiffFactory.withURL(url);

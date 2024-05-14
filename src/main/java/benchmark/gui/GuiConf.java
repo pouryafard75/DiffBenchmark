59a56fa23cec649dac6be9a1186230d6a6d44f09
Pouryafard75 <pouryafard75@gmail.com>
2024-05-13 20:39:29 -0400

    Enable all the tools by default

--- src/main/java/benchmark/gui/GuiConf.java
+++ src/main/java/benchmark/gui/GuiConf.java
@@ -2,20 +2,22 @@ package benchmark.gui;
 
 import benchmark.generators.tools.ASTDiffTool;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /* Created by pourya on 2024-04-29*/
 public class GuiConf {
     public static Set<ASTDiffTool> enabled_tools = new LinkedHashSet<>();
     public static Set<DiffViewers> enabled_viewers = new LinkedHashSet<>();
     static {
-        enabled_tools.add(ASTDiffTool.GOD);
-        enabled_tools.add(ASTDiffTool.RMD);
+        enabled_tools.addAll(Arrays.asList(ASTDiffTool.values()));
+        enabled_tools.remove(ASTDiffTool.DAT);
 
     }
 
     static {
-        enabled_viewers.add(DiffViewers.MONACO);
+//        enabled_viewers.add(DiffViewers.MONACO);
         enabled_viewers.add(DiffViewers.VANILLA);
     }
 

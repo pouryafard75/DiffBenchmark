ee97204aa3bfcd5547a3c6f5e21fa3a0ce2fc69e
Pouryafard75 <pouryafard75@gmail.com>
2024-05-17 10:24:21 -0400

    RQ Driver to execute all

---
+++ src/main/java/rq/RQDriver.java
@@ -0,0 +1,33 @@
+package rq;
+
+import benchmark.metrics.computers.filters.MappingsTypeFilter;
+import benchmark.utils.Configuration.Configuration;
+import benchmark.utils.Configuration.ConfigurationFactory;
+
+/* Created by pourya on 2024-05-17*/
+public class RQDriver {
+    public static void main(String[] args) {
+        Configuration[] confs = {ConfigurationFactory.hack()};
+        RQ[] rqs = {
+                new RQ1(),
+                new RQ2(),
+                new RQ3(MappingsTypeFilter.PROGRAM_ELEMENTS),
+                new RQ3(MappingsTypeFilter.FIELD_DECL_ONLY),
+                new RQ3(MappingsTypeFilter.METHOD_DECL_ONLY),
+                new RQ3(MappingsTypeFilter.TYPE_DECL_ONLY),
+                new RQ3(MappingsTypeFilter.ENUM_DECL_ONLY),
+                new RQ4(),
+                new RQ5(),
+                new RQ6(),
+                new RQ7(),
+                /*new RQ8(), */  /*Skip the execution time RQ in the driver.*/
+        };
+        for (RQ rq : rqs) {
+            try {
+                rq.run(confs);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}

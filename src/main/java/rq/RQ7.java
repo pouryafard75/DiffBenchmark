9aa1b6e2830c192dacbcf3547603d4507523cac1
Pouryafard75 <pouryafard75@gmail.com>
2024-05-17 10:24:00 -0400

    Write RQ7 to file

--- src/main/java/rq/RQ7.java
+++ src/main/java/rq/RQ7.java
@@ -7,7 +7,11 @@ import benchmark.generators.tools.ASTDiffTool;
 import benchmark.utils.Configuration.Configuration;
 import benchmark.utils.Configuration.ConfigurationFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 /***
@@ -29,6 +33,34 @@ public class RQ7 implements RQ  {
 
     private static void rq7(Configuration configuration) throws IOException {
         Map<ASTDiffTool, Integer> astDiffToolIntegerMap = new CommitPerfectRatioBenchmarkComputer(configuration).perfectRatio();
-        System.out.println(astDiffToolIntegerMap);
+        writeToFile(astDiffToolIntegerMap, "out/rq7-" + configuration.getName() + ".txt");
+    }
+    static void writeToFile(Map<ASTDiffTool, Integer> astDiffToolIntegerMap, String outputFilePath) {
+        File file = new File(outputFilePath);
+
+        BufferedWriter bf = null;
+        try {
+            bf = new BufferedWriter(new FileWriter(file));
+            // iterate map entries
+            for (Map.Entry<ASTDiffTool, Integer> entry :
+                    astDiffToolIntegerMap.entrySet()) {
+                // put key and value separated by a colon
+                bf.write(entry.getKey().getToolName() + ":"
+                        + entry.getValue());
+                // new line
+                bf.newLine();
+            }
+            bf.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                bf.close();
+            }
+            catch (Exception e) {
+            }
+        }
     }
 }

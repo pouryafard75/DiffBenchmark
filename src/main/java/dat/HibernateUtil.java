7b8255ad657b8d4a3e5b3305f6d52ad46aa90c2a
Pouryafard75 <pouryafard75@gmail.com>
2024-06-24 21:29:05 -0400

    Hibernate config files

---
+++ src/main/java/dat/HibernateUtil.java
@@ -0,0 +1,33 @@
+package dat;
+
+/* Created by pourya on 2024-06-24*/
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HibernateUtil {
+    private final static Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            logger.error("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+}

9db5f576393228c4229c3ade2ddaecd93f90f6bb
Pouryafard75 <pouryafard75@gmail.com>
2024-06-24 22:51:58 -0400

    Modify the default tools

--- src/main/java/benchmark/gui/GuiConf.java
+++ src/main/java/benchmark/gui/GuiConf.java
@@ -11,13 +11,19 @@ public class GuiConf {
     public static Set<ASTDiffTool> enabled_tools = new LinkedHashSet<>();
     public static Set<DiffViewers> enabled_viewers = new LinkedHashSet<>();
     static {
-        enabled_tools.addAll(Arrays.asList(ASTDiffTool.values()));
-        enabled_tools.remove(ASTDiffTool.DAT);
+        enabled_tools.add(ASTDiffTool.GOD);
+        enabled_tools.add(ASTDiffTool.RMD);
+        enabled_tools.add(ASTDiffTool.GTG);
+        enabled_tools.add(ASTDiffTool.GTS);
+        enabled_tools.add(ASTDiffTool.TRV);
+
+//        enabled_tools.addAll(Arrays.asList(ASTDiffTool.values()));
+//        enabled_tools.remove(ASTDiffTool.DAT);
 
     }
 
     static {
-//        enabled_viewers.add(DiffViewers.MONACO);
+        enabled_viewers.add(DiffViewers.MONACO);
         enabled_viewers.add(DiffViewers.VANILLA);
     }
 

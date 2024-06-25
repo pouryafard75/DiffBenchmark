b283e918b2780b2b4564d84316813d0c90e6fd99
Pouryafard75 <pouryafard75@gmail.com>
2024-06-25 01:04:39 -0400

    Revert "Make HibernateUtil.java runnable to capture the bug on server"
    This reverts commit e299d9500d192882cbeeaf3d81e3634b40994df2.

--- src/main/java/dat/HibernateUtil.java
+++ src/main/java/dat/HibernateUtil.java
@@ -10,14 +10,11 @@ public class HibernateUtil {
     private final static Logger logger = LoggerFactory.getLogger(HibernateUtil.class);
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
-    public static void main(String[] args) {
-        HibernateUtil.buildSessionFactory();
-    }
 
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+            return new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             logger.error("Initial SessionFactory creation failed." + ex);

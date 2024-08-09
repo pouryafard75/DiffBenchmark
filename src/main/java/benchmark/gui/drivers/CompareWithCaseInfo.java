cab5b4a3ca6adff0294a12189bca4b8132ebc9c1
Pouryafard75 <pouryafard75@gmail.com>
2024-08-09 01:05:13 -0400

    Better naming

--- src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
+++ src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
@@ -3,10 +3,9 @@ package benchmark.gui.drivers;
 import benchmark.gui.web.BenchmarkWebDiff;
 import benchmark.gui.web.BenchmarkWebDiffFactory;
 import benchmark.utils.CaseInfo;
-import benchmark.utils.Configuration.Configuration;
 
 /* Created by pourya on 2024-08-09*/
-public class CompareWithCaseInfos {
+public class CompareWithCaseInfo {
     public static void main(String[] args) {
         CaseInfo info = new CaseInfo("Lang", "21");
         BenchmarkWebDiff benchmarkWebDiff = null;

9e7dae9966f6d4a6004320df38157492adef5f6c
Pouryafard75 <pouryafard75@gmail.com>
2024-08-09 01:06:32 -0400

    Minor improvement

--- src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
+++ src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
@@ -8,6 +8,8 @@ import benchmark.utils.CaseInfo;
 public class CompareWithCaseInfo {
     public static void main(String[] args) {
         CaseInfo info = new CaseInfo("Lang", "21");
+        //Or you can pass it via url as follows:
+        //CaseInfo info = new CaseInfo("https://github.com/Alluxio/alluxio/commit/9aeefcd8120bb3b89cdb437d8c32d2ed84b8a825")
         BenchmarkWebDiff benchmarkWebDiff = null;
         try {
             benchmarkWebDiff = new BenchmarkWebDiffFactory().withCaseInfo(info);

e04cc9ee812638312f3d95fea2775b8b572b3861
Pouryafard75 <pouryafard75@gmail.com>
2024-08-08 23:32:36 -0400

    Enable the refactoringminer path to be coming from env variables

--- src/main/java/benchmark/utils/Configuration/ConfigurationFactory.java
+++ src/main/java/benchmark/utils/Configuration/ConfigurationFactory.java
@@ -9,7 +9,7 @@ public class ConfigurationFactory {
     // Update value to the RefactoringMiner cloned repository path in your hard drive
     private static final String REFACTORING_MINER_PATH = "/Users/pourya/IdeaProjects/RM-ASTDiff/";
 
-    public static final String FINALIZED_REFACTORING_MINER_PATH = System.getProperty("refactoringMinerPath", REFACTORING_MINER_PATH);
+    public static final String FINALIZED_REFACTORING_MINER_PATH = System.getProperty("REFACTORINGMINER_PATH", REFACTORING_MINER_PATH);
 
     public static final String ORACLE_DIR = FINALIZED_REFACTORING_MINER_PATH + "/src/test/resources/oracle/commits/";
 

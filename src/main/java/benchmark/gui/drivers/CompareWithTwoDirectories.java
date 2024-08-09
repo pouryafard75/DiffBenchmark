f82e97090f12b62c475a6e9ec09f98b1cf9ac147
Pouryafard75 <pouryafard75@gmail.com>
2024-08-09 00:19:11 -0400

    Fix the CompareWithTwoDirectories

--- src/main/java/benchmark/gui/drivers/CompareWithTwoDirectories.java
+++ src/main/java/benchmark/gui/drivers/CompareWithTwoDirectories.java
@@ -8,18 +8,11 @@ import java.io.IOException;
 
 public class CompareWithTwoDirectories {
     public static void main(String[] args) throws IOException {
-        final String projectRoot = System.getProperty("user.dir");
-        String folder1 = projectRoot + "/tmp/v1/";
-        String folder2 = projectRoot + "/tmp/v2/";
-
-        folder1 = "/Users/pourya/IdeaProjects/RM-ASTDiff/src/test/resources/oracle/commits/defects4j/before/Lang/20/";
-        folder2 = "/Users/pourya/IdeaProjects/RM-ASTDiff/src/test/resources/oracle/commits/defects4j/after/Lang/20/";
-
-        CaseInfo info = new CaseInfo("Lang", "20");
-
+        String folder1 = "PATH_TO_FOLDER1";
+        String folder2 = "PATH_TO_FOLDER2";
         BenchmarkWebDiff benchmarkWebDiff = null;
         try {
-            benchmarkWebDiff = new BenchmarkWebDiffFactory().withTwoDirectories(folder1,folder2, info);
+            benchmarkWebDiff = new BenchmarkWebDiffFactory().withTwoDirectories(folder1,folder2);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);

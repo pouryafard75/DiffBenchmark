ec58a5a3674b7106f83bb65068bc44dfb5cc7cbc
Pouryafard75 <pouryafard75@gmail.com>
2025-03-05 11:48:42 -0500

    Add a better example for compare

--- src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
+++ src/main/java/benchmark/gui/drivers/CompareWithCaseInfo.java
@@ -1,14 +1,12 @@
 package benchmark.gui.drivers;
 
 import benchmark.data.diffcase.D4JCase;
+import benchmark.data.diffcase.GithubCase;
 import benchmark.data.diffcase.IBenchmarkCase;
 import benchmark.data.exp.ToolSets;
 import benchmark.gui.conf.WebDiffConf;
-import benchmark.gui.viewers.DiffViewers;
 import benchmark.gui.web.BenchmarkWebDiff;
 import benchmark.gui.web.BenchmarkWebDiffFactory;
-
-import java.util.Set;
 
 /* Created by pourya on 2024-08-09*/
 public class CompareWithCaseInfo {
@@ -16,9 +14,9 @@ public class CompareWithCaseInfo {
     public static final WebDiffConf webDiffConf = WebDiffConf.defaultConf();
 
     public static void main(String[] args) {
-        IBenchmarkCase info = new D4JCase("Closure", "22");
-        webDiffConf.setEnabled_tools(ToolSets.LITERATURE_TOOLS);
-        webDiffConf.setEnabled_viewers(Set.of(DiffViewers.VANILLA));
+        IBenchmarkCase info = new GithubCase("https://github.com/pouryafard75/TestCases/commit/4e31fb03d9e9d67f3b3dd6ea2c1703551deb54a0");
+        info = new D4JCase("Codec", "13");
+        webDiffConf.setEnabled_tools(ToolSets.PERFECTION_BATTLE);
 
 //        {"repo":"Closure","commit":"22"}]
         //Or you can pass it via url as follows:

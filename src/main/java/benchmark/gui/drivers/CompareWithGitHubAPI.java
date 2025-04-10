77f7418e424e11b165f6c9c97967aabee236b45f
Pouryafard75 <pouryafard75@gmail.com>
2025-04-10 11:32:05 -0400

    Driver updated

--- src/main/java/benchmark/gui/drivers/CompareWithGitHubAPI.java
+++ src/main/java/benchmark/gui/drivers/CompareWithGitHubAPI.java
@@ -2,40 +2,47 @@ package benchmark.gui.drivers;
 
 import benchmark.data.diffcase.GithubCase;
 import benchmark.data.exp.ToolSets;
+import benchmark.generators.tools.ASTDiffToolEnum;
 import benchmark.gui.conf.WebDiffConf;
 import benchmark.gui.web.BenchmarkWebDiff;
 import benchmark.gui.web.BenchmarkWebDiffFactory;
 
 import java.util.LinkedHashSet;
 
+import static benchmark.generators.tools.runners.Utils.writeAll;
+
 /* Created by pourya on 2022-12-26 9:30 p.m. */
 public class CompareWithGitHubAPI {
     public static void main(String[] args) throws Exception {
         String url = "https://github.com/Alluxio/alluxio/commit/9aeefcd8120bb3b89cdb437d8c32d2ed84b8a825";
-        url = "https://github.com/pouryafard75/TestCases/commit/383a2a200e1272a4171849e813efa3f7287b7024";
-        url = "https://github.com/pouryafard75/TestCases/commit/4e31fb03d9e9d67f3b3dd6ea2c1703551deb54a0";
-        url = "https://github.com/JetBrains/intellij-community/commit/9fbf6b8";
-//        url = "https://github.com/pouryafard75/TestCases/commit/438c21418f843cadc115f9c01eedd28de2ebd280";
-//        url = "https://github.com/zeromq/jeromq/commit/02d3fa171d02c9d82c7bdcaeb739f47d0c0006a0";
-//        url = "https://github.com/junit-team/junit5/commit/6b575f2ee5f02288a774ff0a85ce3a3e3cb6946f";
-//        url = "https://github.com/google/truth/commit/1768840bf1e69892fd2a23776817f620edfed536";
+        url = "https://github.com/processing/processing/commit/f36b736cf1206dd1af794d6fb4cee967a3553b1f";
+        url = "https://github.com/JetBrains/intellij-community/commit/8d7a26edd1fedb9505b4f2b4fe57b2d2958b4dd9";
+        url = "https://github.com/google/truth/commit/1768840bf1e69892fd2a23776817f620edfed536";
+        url = "https://github.com/phishman3579/java-algorithms-implementation/commit/ab98bcacf6e5bf1c3a06f6bcca68f178f880ffc9";
+        url = "https://github.com/fabric8io/fabric8/commit/8127b21a220ca677c4e59961d019e7753da7ea6e";
+        url = "https://github.com/eclipse/jetty.project/commit/837d1a74bb7d694220644a2539c4440ce55462cf";
+        url = "https://github.com/jetty/jetty.project/commit/837d1a74bb7d694220644a2539c4440ce55462cf";
+//        url = "https://github.com/pouryafard75/TestCases/commit/4383fc88c49244e1ac77309075570e6396ee9e70";
+        url = "https://github.com/processing/processing/commit/acf67c8cb58d13827e14bbeeec11a66f9277015f";
+
         WebDiffConf webDiffConf = WebDiffConf.defaultConf();
         webDiffConf.setEnabled_tools(ToolSets.ALL);
         webDiffConf.setEnabled_tools(ToolSets.BeforeAndAfterTranslations);
         webDiffConf.setEnabled_tools(ToolSets.VISITOR_EXP_BATTLE_TOOLS);
         webDiffConf.setEnabled_tools(new LinkedHashSet<>()
              {{
-//                 this.add(ASTDiffToolEnum.GOD);
-//                 this.add(ASTDiffToolEnum.GOD);
-//                 this.add(ASTDiffToolEnum.SPN_T);
-//                 this.add(ASTDiffToolEnum.SPN_G_);
+                 this.add(ASTDiffToolEnum.RMD);
+//                 this.add(ASTDiffToolEnum.GTG);
+//                 this.add(ASTDiffToolEnum.GTS);
+                 this.add(ASTDiffToolEnum.SPN_T);
+                 this.add(ASTDiffToolEnum.SPN_I);
 //                 this.add(ASTDiffToolEnum.SPN_G_T);
-//                 this.add(ASTDiffToolEnum.SPN_G_T);
-//                 this.add(ASTDiffToolEnum.SPN_S_);
+//                 this.add(ASTDiffToolEnum.SPN_G_I);
 //                 this.add(ASTDiffToolEnum.SPN_S_T);
+//                 this.add(ASTDiffToolEnum.SPN_S_I);
              }}
         );
-        webDiffConf.setEnabled_tools(ToolSets.ALL);
+
 //        webDiffConf.setEnabled_viewers(Set.of(DiffViewers.VANILLA));
         BenchmarkWebDiff benchmarkWebDiff = new BenchmarkWebDiffFactory(webDiffConf)
 
